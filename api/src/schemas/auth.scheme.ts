267211c44d0e30a3a3a61615e63742e0b7cf3352
spideyzac <zacmlesser-7@outlook.com>
2024-10-28 17:06:52 -0700

    fix(api): more robust schemas

--- api/src/schemas/auth.scheme.ts
+++ api/src/schemas/auth.scheme.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const SignupScheme = z.object({
-    email: z.string().email(),
+    email: z.string().email().max(255),
     password: z.string().min(8).max(255),
     username: z.string().min(3).max(16),
 });
 
 export const LoginScheme = z.object({
-    user: z.string(),
-    password: z.string(),
+    user: z.string().max(16),
+    password: z.string().max(255),
 });
